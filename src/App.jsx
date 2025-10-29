839a0be00f91195846d4b055de5d44f73a11c598
shrestha <shresthachakrabortykgp@gmail.com>
2025-10-30 00:17:38 +0530

    vite setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+import "./index.css";
+function App() {
+  return (
+    <>
+      <div className="text-blue-500">hi</div>
+    </>
+  );
+}
+
+export default App;
