300fe6deb7e4e8f19f11a1a8b2978408b065ec32
shrestha <shresthachakrabortykgp@gmail.com>
2025-10-30 00:51:51 +0530

    render in the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import FormBuilder from "./formBuilder";
 
-import "./index.css";
 function App() {
+  const [schema, setSchema] = useState([]);
+
   return (
-    <>
-      <div className="text-blue-500">hi</div>
-    </>
+    <div style={{ padding: 20 }}>
+      <h1 style={{ textAlign: "center" }}>Dynamic Form Builder</h1>
+      <FormBuilder onSchemaChange={(fields) => setSchema(fields)} />
+
+      {/* <pre style={{ marginTop: 40, background: "#eee", padding: 10 }}>
+        <strong>Live JSON Schema:</strong>
+        <br />
+        {JSON.stringify({ fields: schema }, null, 2)}
+      </pre> */}
+    </div>
   );
 }
 
