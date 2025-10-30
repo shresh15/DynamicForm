2fb8040380b91bd55c7bd6271d957df41cbae019
shrestha <shresthachakrabortykgp@gmail.com>
2025-10-30 11:40:21 +0530

    testing the firebase connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import FormBuilder from "./formBuilder";
+import TestFirestore from "./TestFirestore";
 
 function App() {
   const [schema, setSchema] = useState([]);
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ textAlign: "center" }}>Dynamic Form Builder</h1>
-      <FormBuilder onSchemaChange={(fields) => setSchema(fields)} />
+    // <div style={{ padding: 20 }}>
+    //   <h1 style={{ textAlign: "center" }}>Dynamic Form Builder</h1>
+    //   <FormBuilder onSchemaChange={(fields) => setSchema(fields)} />
 
-      {/* <pre style={{ marginTop: 40, background: "#eee", padding: 10 }}>
-        <strong>Live JSON Schema:</strong>
-        <br />
-        {JSON.stringify({ fields: schema }, null, 2)}
-      </pre> */}
-    </div>
+    //   {/* <pre style={{ marginTop: 40, background: "#eee", padding: 10 }}>
+    //     <strong>Live JSON Schema:</strong>
+    //     <br />
+    //     {JSON.stringify({ fields: schema }, null, 2)}
+    //   </pre> */}
+    // </div>
+    <TestFirestore />
   );
 }
 
